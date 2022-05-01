6bc64264660ddae13c36af286af0f3591571b073
Prathamesh Moharkar <prathameshmoharkar125@gmail.com>
2022-05-02 01:10:20 +0530

    Homepage 90% Complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
+      <Home />
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Carousel from "./Carousel/Carousel";
 import Sections from "./Sections/Sections";
+import Footer from "./Footer/Footer";
 const Home = () => {
   return (
-    <div className="overflow-x-hidden h-[200rem]">
+    <div className="overflow-x-hidden " id="home">
       <Navbar />
       <Carousel />
       <Sections />
+      <Footer />
     </div>
   );
 };
