89069257f5155a5028ebec0dcd41834ba4c60d71
chetan <chetangamne12@gmail.com>
2022-05-03 08:16:23 +0530

    added toggle effect between login.js and register.js done with tailwind for now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 import Navbar from "./Navbar/Navbar";
+import Login from "../auth/Login";
 import Carousel from "./Carousel/Carousel";
 import Sections from "./Sections/Sections";
-<<<<<<< HEAD
-import fade from "react-reveal/Fade";
 
-||||||| 4137cbd
-=======
-import Login from "../auth/Login";
-import Contact from "../Contact/Contact";
-// import { useState } from "react";
-import Footer from "./Footer/Footer";
->>>>>>> f5d4f20f1e91cfe2c82e26e96f4982ea9eb7c741
 const Home = () => {
   const [loginOpen, setLoginOpen] = useState(false);
   return (
-<<<<<<< HEAD
     <div className="overflow-x-hidden h-[200rem]">
-      <Navbar />
-      <Carousel />
-      <fade>
-        <Sections />
-      </fade>
-||||||| 4137cbd
-    <div className="overflow-x-hidden h-[200rem]">
-      <Navbar />
-      <Carousel />
-      <Sections />
-=======
-    <div className="overflow-x-hidden " id="home">
-      <Navbar setlogin={setLoginOpen} />
+       <Navbar setlogin={setLoginOpen}/>
       {loginOpen ? <Login setlogin={setLoginOpen} /> : console.log("close")}
-      {/* <Carousel />
+      <Carousel />
       <Sections />
-      <Footer /> */}
-      <Contact />
->>>>>>> f5d4f20f1e91cfe2c82e26e96f4982ea9eb7c741
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,13 @@ module.exports = {
     "./src/**/*.{html,js}",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'login-pattern': "url('/src/assets/fill_form2.svg')",
+        
+      }
+    }
+  },
   plugins: [require("tw-elements/dist/plugin")],
 };
