e56eec15a5aad3008c1e984401ba8742a841fd71
chetan <chetangamne12@gmail.com>
2022-05-03 11:35:27 +0530

    Done with product.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
+import Product from "./components/Products/Product";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Login />
+      <Home />
+      {/* <Login /> */}
+      {/* <Product /> */}
       {/* <Register /> */}
     </>
   );
